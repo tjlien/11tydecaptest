9c111bd85192fc0c9c08d2ea30ba19cafc62c381
TJ Lien <89636647+tjlien@users.noreply.github.com>
2024-04-28 14:13:50 -0700

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/img");
+    eleventyConfig.addPassthroughCopy("./src/fonts");
+    eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addPassthroughCopy("admin");
+  
+    return {
+      passthroughFileCopy: true,
+      dir: {
+        input: "src",
+        output: "public",
+        includes: "_includes",
+      },
+    };
+  };
